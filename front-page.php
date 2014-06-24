43559600180ca6477fa1ae4b9f86249d3bf015c2
toonya <msc5762@me.com>
2014-06-24 11:15:16 +0800

    save homepage done section topic.

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="banner">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="4000">
+    <div id="carousel-banner" class="carousel slide" data-ride="carousel" data-interval="4000">
       <!-- Indicators -->
       <ol class="carousel-indicators">
           <?php
@@ -16,10 +16,10 @@
       </div>
 
       <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+      <a class="left carousel-control" href="#carousel-banner" data-slide="prev">
           <span class="glyphicon glyphicon-chevron-left"></span>
       </a>
-      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+      <a class="right carousel-control" href="#carousel-banner" data-slide="next">
           <span class="glyphicon glyphicon-chevron-right"></span>
       </a>
     </div>
@@ -30,15 +30,15 @@
 	<div class="container">
 		<div class="strong-type">
 		    <div class="items">
-		        <h3>个性</h3>
+		        <h3><img src="<?php echo get_template_directory_uri(); ?>/images/title01.jpg" alt=""> 个性</h3>
 		        <p>更大、更私密空间， 能够满足您不同场景下的个性化需求。</p>
 		    </div>
 		    <div class="items">
-		        <h3>品质</h3>
+		        <h3><img src="<?php echo get_template_directory_uri(); ?>/images/title03.jpg" alt=""> 品质</h3>
 		        <p>专人定期现场检查，保证无与伦比的住宿品质。</p>
 		    </div>
 		    <div class="items">
-		        <h3>超值</h3>
+		        <h3><img src="<?php echo get_template_directory_uri(); ?>/images/title02.jpg" alt=""> 超值</h3>
 		        <p>人均价格及附加服务使得别墅比高端酒店性价比更高。</p>
 		    </div>
 		    <div class="clearfix"></div>
@@ -46,5 +46,64 @@
 	</div>
 </section>
 
+<section class="preferential">
+	<div class="container">
+		<div id="carousel-preferential" class="carousel slide" data-ride="carousel" data-interval="">
+		  
+		  <div class="section-header text-center">
+		  	<!-- Controls -->
+		  	<a class="left carousel-control" href="#carousel-preferential" data-slide="prev">
+		  	    <span class="bg-icon-gray-left"></span>
+		  	</a>
+		  	<a class="right carousel-control" href="#carousel-preferential" data-slide="next">
+		  	    <span class="bg-icon-gray-right"></span>
+		  	</a>
+		  	<h1>限时优惠</h1>
+		  </div>
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+		      <div class="item active">
+		      	<div class="row">
+		      		<div class="col-sm-4"><a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/0.jpg" alt=""></a></div>
+		      		<div class="col-sm-4"><a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/0(1).jpg" alt=""></a></div>
+		      		<div class="col-sm-4"><a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/0(2).jpg" alt=""></a></div>
+		      	</div>
+		      </div>
+		      <div class="item">
+		      	<div class="row">
+		      		<div class="col-sm-4"><a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/0.jpg" alt=""></a></div>
+		      		<div class="col-sm-4"><a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/0(1).jpg" alt=""></a></div>
+		      		<div class="col-sm-4"><a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/0(2).jpg" alt=""></a></div>
+		      	</div>
+		      </div>
+		  </div>
+		</div>
+	</div>
+</section>
+
+<section class="topic">
+	<div class="container">
+		<div class="section-header">
+			<h1>游学主题</h1>
+		</div>
+		<div class="row">
+			<div class="col-sm-7">
+				<div class="row">
+					<div class="col-sm-6">
+						<a class="img-resp-block" href="#"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/20140505182129GhMImN.jpg"></a>
+					</div>
+					<div class="col-sm-6">
+						<a class="img-resp-block" href="#"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/20140505182153VCdYqR.jpg" class="img-responsive"></a>
+					</div>
+				</div>
+				<div class="row-block"></div>
+				<a class="img-resp-block" href="#"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/20140505182212tqyzXA.jpg"></a>
+			</div>
+			<div class="col-sm-5">
+				<a class="img-resp-block" href="#"><img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/uploads/20140505182239kWdisj.jpg"></a>
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php get_footer('home')?>
