05106b4d56d547063feaecbfad9bedda4fa1bf57
toonya <msc5762@me.com>
2014-06-24 14:59:24 +0800

    adjust footer grid.

--- footer-home.php
+++ footer-home.php
@@ -1,11 +1,15 @@
 			<footer>
 				<div class="container">
 					<div class="row">
-						<div class="col-md-2">
-							<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/logo-sm.png" alt="">
-						</div>
-						<div class="col-md-4">
-							<p>Sense Luxury是顶级奢华别墅度假的专家。我们推崇与众不同、奢华体验的度假方式。我们从全球范围精选了顶级度假目的地：岛屿、乡野别墅、沙漠露营等。每一处都能满足您对精致住宿体验的所有幻想！在这里旅行者总能找到全球各地最令人惊叹的奢华别墅。而我们也可以根据旅行者的要求提供最为专业的一站式服务。</p>
+						<div class="col-md-6">
+							<div class="row">
+								<div class="col-md-4">
+									<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/logo-sm.png" alt="">
+								</div>
+								<div class="col-md-7">
+									<p>Sense Luxury是顶级奢华别墅度假的专家。我们推崇与众不同、奢华体验的度假方式。我们从全球范围精选了顶级度假目的地：岛屿、乡野别墅、沙漠露营等。每一处都能满足您对精致住宿体验的所有幻想！在这里旅行者总能找到全球各地最令人惊叹的奢华别墅。而我们也可以根据旅行者的要求提供最为专业的一站式服务。</p>
+								</div>
+							</div>
 						</div>
 						<div class="col-md-4">
 							<div class="row">
