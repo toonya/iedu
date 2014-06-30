2aceebf92832dfc1fe0640168910765df9f59cc1
toonya <msc5762@me.com>
2014-06-30 23:02:45 +0800

    visit

--- inc/post-type-visit.php
+++ inc/post-type-visit.php
@@ -5,9 +5,9 @@
 // ----------------------------------------
 function iedu_visit() {
   $default = array(
-		'name' => '国际教育探访团',
-		'slug' => 'iedu_visit'
-	);
+    'name' => '国际教育探访团',
+    'slug' => 'iedu_visit'
+  );
 
   $labels = array(
     'name' => $default['name'],
@@ -32,9 +32,9 @@ function iedu_visit() {
     'show_ui' => true,
     'show_in_menu' => true,
     'query_var' => true,
-	//Provide a callback function that will be called when setting up the meta boxes for the edit form. Do remove_meta_box() and add_meta_box() calls in the callback.
-	  'register_meta_box_cb' => 'add_visit_metaboxs',
-	//'with_front' => bool Should the permastruct be prepended with the front base. (example: if your permalink structure is /blog/, then your links will be: false->/news/, true->/blog/news/). Defaults to true
+  //Provide a callback function that will be called when setting up the meta boxes for the edit form. Do remove_meta_box() and add_meta_box() calls in the callback.
+    'register_meta_box_cb' => 'add_visit_metaboxs',
+  //'with_front' => bool Should the permastruct be prepended with the front base. (example: if your permalink structure is /blog/, then your links will be: false->/news/, true->/blog/news/). Defaults to true
     'rewrite' => array( 'slug' => $default['slug'] ),
     'capability_type' => 'post',
     'has_archive' => true,
@@ -46,7 +46,7 @@ function iedu_visit() {
   register_post_type( $default['slug'], $args );
 }
 if(!post_type_exists('iedu_visit'))
-	add_action( 'init', 'iedu_visit' );
+  add_action( 'init', 'iedu_visit' );
 
 
 // ----------------------------------------
@@ -62,6 +62,7 @@ function get_visit_template($single_template) {
      return $single_template;
 }
 //add_filter( 'single_template', 'get_visit_template' );
+
 
 
 // ----------------------------------------
@@ -81,6 +82,15 @@ function add_visit_metaboxs() {
       'normal',
       'core'
     );
+
+    add_meta_box(
+        'showcase'
+        ,'相片集'
+        ,'render_visit_showcase'
+        , $screen
+        ,'normal'
+        ,'core'
+      );
   }
 }
 
@@ -88,59 +98,59 @@ function add_visit_metaboxs() {
 //$visit_prefix = 'visit_';
 $visit_meta_fields = array(
   array(
-    'label' => '地址',
-    'desc'  => '',
-    'id'  => 'location',
-    'type'  => 'text'
-  ),
-  array(
-    'label' => '教学系统',
-    'desc'  => '',
-    'id'  => 'edu_sys',
-    'type'  => 'text'
-  ),
-  array(
-    'label' => '课程介绍',
-    'desc'  => '',
-    'id'  => 'course_intro',
-    'type'  => 'textarea'
-  ),
-  array(
-    'label' => '学费',
-    'desc'  => '',
-    'id'  => 'fee',
-    'type'  => 'text'
-  ),
-  array(
-    'label' => '教学&详情',
-    'desc'  => '',
-    'id'  => 'edu_info',
-    'type'  => 'textarea'
-  ),
-  array(
-    'label' => '入学&要求',
-    'desc'  => '',
-    'id'  => 'requirement',
-    'type'  => 'textarea'
-  ),
-  array(
-    'label' => '费用&条款',
-    'desc'  => '',
-    'id'  => 'fee_agreement',
-    'type'  => 'textarea'
-  ),
-  array(
-    'label' => '服务流程',
-    'desc'  => '',
-    'id'  => 'service_process',
-    'type'  => 'textarea'
-  ),
-  array(
-    'label' => '申请表格',
-    'desc'  => '',
-    'id'  => 'apply_form',
-    'type'  => 'textarea'
-  ),
+      'label' => '地址',
+      'desc'  => '',
+      'id'  => 'location',
+      'type'  => 'text'
+    ),
+    array(
+      'label' => '探访时间',
+      'desc'  => '',
+      'id'  => 'time',
+      'type'  => 'text'
+    ),
+    array(
+      'label' => '价格',
+      'desc'  => '',
+      'id'  => 'fee',
+      'type'  => 'text'
+    ),
+    array(
+      'label' => '参加人数',
+      'desc'  => '',
+      'id'  => 'num_of_people',
+      'type'  => 'text'
+    ),
+    array(
+      'label' => '探访学校数量',
+      'desc'  => '',
+      'id'  => 'num_of_school',
+      'type'  => 'text'
+    ),
+    array(
+      'label' => '探访&详情',
+      'desc'  => '',
+      'id'  => 'visit_detail',
+      'type'  => 'textarea'
+    ),
+    array(
+      'label' => '价格&条款',
+      'desc'  => '',
+      'id'  => 'fee_agreement',
+      'type'  => 'textarea'
+    ),
+    array(
+      'label' => '服务流程',
+      'desc'  => '',
+      'id'  => 'service_process',
+      'type'  => 'textarea'
+    ),
+    array(
+      'label' => '申请材料',
+      'desc'  => '',
+      'id'  => 'apply_form',
+      'type'  => 'textarea'
+    ),
 );
 
 
@@ -181,6 +191,50 @@ function visit_meta_box_callback( $post ) {
     echo '</div>';
   }
   echo '</div>';
+}
+
+// ----------------------------------------
+// ! render_showcase
+// ----------------------------------------
+function render_visit_showcase($post) {
+
+  wp_nonce_field( 'visit_showcase', 'visit_showcase_nonce' );
+
+  /*
+   * Use get_post_meta() to retrieve an existing value
+   * from the database and use the value for the form.
+   */
+  $value = get_post_meta( $post->ID, 'showcase', true );
+  ?>
+  <div class="row">
+    <div class="funtions col-xs-11">
+      <div class="btn-group btn-group-justified">
+        <a href="#" class="btn btn-default disable new">新增</a>
+        <a href="#" class="btn btn-default disable edit-close">编辑</a>
+      </div>
+      <div class="hidden item template">
+          <input type="hidden" id="showcase" name="" value="" />
+          <div class="edit-area hidden">
+            <button type="button" class="close">×</button>
+          </div>
+          <img src="" alt="" />
+      </div>
+
+      <div class="showcase"><!--
+        <?php if($value): foreach($value as $key=>$url){ ?>
+          --><div class="item">
+              <input type="hidden" id="showcase" name="showcase[<?php echo $key; ?>]" value="<?php echo esc_attr($url); ?>" />
+              <div class="edit-area hidden">
+                <button type="button" class="close">×</button>
+              </div>
+              <img src="<?php echo esc_attr($url); ?>" alt="" />
+          </div><!--
+        <?php } endif;?>
+      --></div>
+    </div>
+    <div class="clear"></div>
+  </div>
+  <?php
 }
 
 /**
@@ -245,6 +299,19 @@ function visit_save_meta_box_data( $post_id ) {
       delete_post_meta($post_id, $field['id'], $old);
     }
   } // enf foreach
+
+  if( !empty($_POST['showcase']) ) {
+    $mydata =  $_POST['showcase'] ;
+
+    // Update the meta field in the database.
+    update_post_meta( $post_id, 'showcase', $mydata );
+  }
+  else {
+    $mydata = '';
+
+    // Update the meta field in the database.
+    update_post_meta( $post_id, 'showcase', $mydata );
+  }
 
   // Sanitize user input.
   //$my_data = sanitize_text_field( $_POST['visit'] );
