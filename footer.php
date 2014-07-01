a6407b0ed1040d7ea88e9bfcede35976ddcc3d80
toonya <msc5762@me.com>
2014-07-01 14:44:14 +0800

    sidebar contact area

--- footer.php
+++ footer.php
@@ -49,6 +49,8 @@
 				</div></div>
 			</footer>
 
+			<?php get_sidebar('contact'); ?>
+
 		</div>
     	<script src="<?php echo get_template_directory_uri()?>/js/vendor/jquery-1.11.0.min.js"></script>
     	
---
+++ sidebar-contact.php
@@ -0,0 +1,25 @@
+<div class="sidebar-contact">
+	<div class="item-list">
+		<div class="item">
+			<a href="#" class="wx"></a>
+			<div class="pop">
+				<h3>扫描咨询</h3>
+				<div class="qrcode">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/qrcode.png" class="img-responsive" alt="">
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<a href="#" class="qq"></a>
+			<div class="pop">
+				<a href="">点击咨询</a>
+			</div>
+		</div>
+		<div class="item">
+			<a href="#" class="phone"></a>
+			<div class="pop">
+				<a href="tel:">400-400-400</a>
+			</div>
+		</div>
+	</div>
+</div>
