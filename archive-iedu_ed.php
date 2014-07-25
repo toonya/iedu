11571873456aa328d85a13ef09950156ca36e0e1
toonya <msc5762@qq.com>
2014-07-25 17:23:24 +0800

    将所有二级页面与三级页面的背景分开

--- archive-iedu_ed.php
+++ archive-iedu_ed.php
@@ -17,7 +17,7 @@
 	</div>
 </div>
 
-<div class="primary custom-post-type <?php echo get_post_type(); ?>">
+<div class="primary custom-post-type archive_<?php echo get_post_type(); ?>">
 	<div class="container">
     	<div class="archive-title"><img src="<?php echo get_template_directory_uri(); ?>/images/title_iedu_ed.png"></div>
 		<div class="row">
