2c9c2c775a4233730edfe24bf6e29a32ad5ab593
toonya <msc5762@me.com>
2014-07-30 11:51:10 +0800

    fix price overflow & the max num of the list.

--- single-iedu_certification.php
+++ single-iedu_certification.php
@@ -43,11 +43,11 @@
 		<div class="row">
 			<div class="col-sm-9">
 				<div class="row">
-					<div class="col-xs-8">
+					<div class="col-xs-6">
 						<h1><?php the_title(); ?></h1>
 						<p class="sec-title"><?php echo $meta['location'] ?></p>
 					</div>
-					<div class="col-xs-4 text-right price">
+					<div class="col-xs-6 text-right price">
 						<strong><?php echo $meta['fee']; ?></strong>/人
 					</div>
 				</div>
