c1a47ec3c9c6da7e3e221d340025ccc573f8c3bb
toonya <msc5762@me.com>
2014-09-04 23:38:59 +0800

    hide news content on mobile device.

--- sidebar-archive.php
+++ sidebar-archive.php
@@ -1,26 +1,40 @@
+
+<?php $news_args = array(
+				   		'post_type' => 'post'
+	               ); 
+	  $news_html = [];
+	  $news_count = 0;
+	  $news_query = new WP_Query($news_args);
+
+	  //print_r($news_query);
+
+	  if ( $news_query->have_posts() ) {
+	  	while ( $news_query->have_posts() ) {
+	  		$news_query->the_post();
+	  		
+	  		$news_html[] = '<div class="news-item"><a href="'.get_permalink().'"><span><i class="fa-rss fa"></i><span class="pd-l-sm">'.get_the_title().'</span></span></a>'.'<span class="time pull-right pd-l-sm">--'.get_the_date('m月d日').'</span><div class="clearfix"></div></div>';
+	  		$news_count = $news_count + 1;
+	  	}
+	  } else {
+	  	
+	  }
+?>
+
 <div class="sidebar">
-	<div class="panel panel-default">
-		<div class="panel-heading">新闻</div>
-		<div class="panel-body">
-		<div class="news-list">
-		<?php $news_args = array(
-						   		'post_type' => 'post'
-			               ); 
-			  $news_query = new WP_Query($news_args);
-
-			  //print_r($news_query);
-
-			  if ( $news_query->have_posts() ) {
-			  	while ( $news_query->have_posts() ) {
-			  		$news_query->the_post();
-			  		
-			  		echo '<div class="news-item"><a href="'.get_permalink().'"><span><i class="fa-rss fa"></i><span class="pd-l-sm">'.get_the_title().'</span></span></a>'.'<span class="time pull-right pd-l-sm">--'.get_the_date('m月d日').'</span><div class="clearfix"></div></div>';
-			  	}
-			  } else {
-			  	
-			  }
-		?>
-		</div> <!-- /. news-list -->
-		</div> 
+	<div class="panel-group" id="accordion">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+					<h4 class="panel-title">新闻 <span class="badge pull-right"><?php echo $news_count ?></span></h4>
+			    </a>
+			</div>
+			<div id="collapseOne" class="panel-collapse collapse <?php if (!wp_is_mobile()) echo 'in'; ?>">
+				<div class="news-list">
+					<?php foreach ($news_html as $key => $news) {		
+						echo $news;
+					} ?>
+				</div> <!-- /. news-list -->
+			</div> 
+		</div>
 	</div>
 </div>
