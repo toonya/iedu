3863c29d7a7f8a1adce35dfeb8451e9a9311bedc
toonya <msc5762@me.com>
2014-09-04 21:54:27 +0800

    divide mobile home page post-type

--- mobile-home.php
+++ mobile-home.php
@@ -1,54 +1,54 @@
-<?php
-
-// The Query
-$mobile_home_post_type = array(
-	'post_type' => array('iedu_school', 'iedu_certification', 'iedu_camp', 'iedu_ed', 'iedu_visit'),
-);
-$the_query = new WP_Query($mobile_home_post_type);
-$if_empty = '';
-$discount_html = [];
-$html_temp = '';
-
-// The Loop
-if ( $the_query->have_posts() ) {
-	while ( $the_query->have_posts() ) {
-		$the_query->the_post();
-		
-		$html_temp = '<div class="item">
-						<a href="'.get_permalink().'">
-							<div class="img-container">
-			      				<img class="img-responsive" src="'.wp_get_attachment_url( get_post_thumbnail_id() ).'" alt="'.get_the_title().'">
-			      			</div>
-			      			<div class="describe">
-			      				<div class="row">
-			      					<div class="col-xs-6">
-			      						<h4>'.get_the_title().'</h4>
-			      					</div>
-			      					<div class="col-xs-6 text-right">
-										<div class="count-down" data-type="countDown" data-time="'.get_post_meta( get_the_ID(), 'discount_finish', true ).'"></div>
-			      					</div>
-			      				</div>
-			      			</div>
-		      			</a>
-		      		</div>';
-
-		$discount_html[] = $html_temp;
-	}
-} else {
-	$if_empty = 'hide';
-}
-
-/* Restore original Post Data */
-wp_reset_postdata();
-?>
-
 <div class="mobile-content">
 	<div class="container">
 		<div class="discount-list">
-			<?php  
-				foreach ($discount_html as $key => $value) {
-					echo $value;
-				}
+			<?php
+
+			// The Query
+			$post_types = array('iedu_school', 'iedu_certification', 'iedu_camp', 'iedu_ed', 'iedu_visit');
+
+			foreach ($post_types as $key => $post_type) {
+				
+				$the_args = array(
+					'post_type' => $post_type,
+				);
+
+				$the_query = new WP_Query($the_args);
+				$the_post_object = get_post_type_object($post_type);
+
+				// The Loop
+				if ( $the_query->have_posts() ) {?>
+
+					<div class="bg-primary post-type-title"><?php echo $the_post_object->labels->singular_name ?></div>
+
+					<?while ( $the_query->have_posts() ) {
+						$the_query->the_post(); ?>
+						
+						<div class="item">
+							<a href="<?php echo get_permalink() ?>">
+								<div class="img-container">
+				      				<img class="img-responsive" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ) ?>" alt="<?php the_title(); ?>">
+				      			</div>
+				      			<div class="describe">
+				      				<div class="row">
+				      					<div class="col-xs-6">
+				      						<h4><?php the_title(); ?></h4>
+				      					</div>
+				      					<div class="col-xs-6 text-right">
+											<div class="count-down" data-type="countDown" data-time="<?php echo get_post_meta( get_the_ID(), 'discount_finish', true ) ?>"></div>
+				      					</div>
+				      				</div>
+				      			</div>
+			      			</a>
+			      		</div>
+
+						<?
+					}
+				} 
+				
+				/* Restore original Post Data */
+				wp_reset_postdata();
+
+			}
 			?>
 		</div>
 	</div>
